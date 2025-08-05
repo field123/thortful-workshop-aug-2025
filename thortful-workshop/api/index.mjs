a75f9e9e0ae00166e735e9e0b7d8fc73ed73a405
Robert Field <robertfield@quadrimular.com>
2025-08-05 13:16:29 +0100

    build: fix function

--- thortful-workshop/api/index.mjs
+++ thortful-workshop/api/index.mjs
@@ -1,3 +1,10 @@
 // api/index.mjs
-export default import('../dist/thortful-workshop/server/server.mjs')
-  .then(m => m.app); // m.app is the Express handler exported by Angular’s server
+let handler;
+
+export default async function(req, res) {
+  if (!handler) {
+    const module = await import('../dist/thortful-workshop/server/server.mjs');
+    handler = module.app;
+  }
+  return handler(req, res);
+}
