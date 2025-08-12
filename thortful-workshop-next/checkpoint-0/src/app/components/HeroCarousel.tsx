8d01ea0de24eaa57003b7238023fd6dadfd0800d
Robert Field <robertfield@quadrimular.com>
2025-08-12 13:10:38 +0100

    feat: updated styles

--- thortful-workshop-next/checkpoint-0/src/app/components/HeroCarousel.tsx
+++ thortful-workshop-next/checkpoint-0/src/app/components/HeroCarousel.tsx
@@ -6,14 +6,13 @@ export default function HeroCarousel() {
     <div className="relative w-full bg-white">
       <Link
         href="/cards/exams/congrats"
-        className="block relative w-full"
-        style={{ height: '350px' }}
+        className="block relative w-full h-[148px] md:h-[268px] lg:h-[390px]"
       >
         <Image
           src="/banner-exam.jpg"
           alt="Exam Congrats w/c 31"
           fill
-          className="object-cover object-center"
+          className="object-fill md:object-fill lg:object-cover object-center"
           priority
           quality={100}
           sizes="100vw"
