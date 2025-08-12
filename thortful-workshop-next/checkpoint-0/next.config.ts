e9f3df2e9e94ce5f57d91a69e60cefac9e9e4eb1
Robert Field <robertfield@quadrimular.com>
2025-08-12 11:22:26 +0100

    feat: cloned site

--- thortful-workshop-next/checkpoint-0/next.config.ts
+++ thortful-workshop-next/checkpoint-0/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.thortful.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-fe.thortful.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'strapi-media.thortful.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
